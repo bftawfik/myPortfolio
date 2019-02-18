1c608aa3f80d27b4787a9d2273e19d5966304245
Bishoy Farouk Tawfik <bftawfik@gmail.com>
2019-02-18 15:42:47 +0200

    adding react router

---
+++ src/BftRoutes.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Container from 'react-bootstrap/Container';
+
+import BftHeader from './components/BftHeader';
+import BftNav from './components/BftNav';
+import BftBio from './components/BftBio';
+import BftAllProjects from './components/BftAllProjects';
+import BftInfoAndContact from './components/BftInfoAndContact';
+
+import  Whoops404 from './components/Whoops404';
+
+const BftRoutes = ()=>(
+    <BrowserRouter>
+      <Container className="App">
+        <Route exact path="*" component={BftHeader}/>
+        <Route exact path="*" component={BftNav}/>
+        <Switch>
+          <Route exact path="/" component={BftBio}/>
+          <Route exact path="/portofolio" component={BftAllProjects}/>
+          <Route exact path="/infoandcontact" component={BftInfoAndContact}/>
+          <Route exact path="*" component={Whoops404}/>
+        </Switch>
+      </Container>
+
+    </BrowserRouter>
+)
+
+export default BftRoutes
+// <Route exact path="/" component={App}/>
+// <Route exact path="/:filter" component={App}/>
+// <Route exact path="/:filter/:filter2" component={App}/>
--- src/components/BftNav.js
+++ src/components/BftNav.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import '../styles/BftNav.sass';
 
 class BftNav extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
+
+
   render() {
     return (
       <div className="BftNav">
-        <h4 className="navLink">Home</h4>
-        <h4 className="navLink">Portofolio</h4>
-        <h4 className="navLink">Info & Contact</h4>
+        <NavLink exact to="/" activeClassName="selected">
+          <h4 className="navLink">Home</h4>
+        </NavLink>
+        <NavLink to="/portofolio" activeClassName="selected">
+          <h4 className="navLink">Portofolio</h4>
+        </NavLink>
+        <NavLink to="/infoandcontact" activeClassName="selected">
+          <h4 className="navLink">Info & Contact</h4>
+        </NavLink>
       </div>
     );
   }
