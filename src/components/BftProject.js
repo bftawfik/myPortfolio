35cd8106d693e399a491e9fc54315798133f6b03
Bishoy Farouk Tawfik <bftawfik@gmail.com>
2019-05-05 15:48:24 +0200

    adding 2 new unfinished projects and fix the project img alt and update data json

--- src/components/BftProject.js
+++ src/components/BftProject.js
@@ -8,15 +8,15 @@ class BftProject extends Component {
     return (
       <div className="BftProject">
         <h4>{this.props.data.title}</h4>
-        <img className="projectImg-xl" src={this.props.data.projectImgPath+"/8-line-height.jpg"} alt="BFTawfik Twitter Bot 2"/>
-        <img className="projectImg-sm" src={this.props.data.projectImgPath+"/5-line-height.jpg"} alt="BFTawfik Twitter Bot 2"/>
+        <img className="projectImg-xl" src={this.props.data.projectImgPath+"/8-line-height.jpg"} alt={this.props.data.projectImgPath}/>
+        <img className="projectImg-sm" src={this.props.data.projectImgPath+"/5-line-height.jpg"} alt={this.props.data.projectImgPath}/>
         <div className="links">
           <div className="tags">
             {this.props.data.tags.map((tag, ndx) => <span key={ndx}><NavLink to={"/portfolio/"+tag.toLowerCase()} activeClassName="selected">{tag}</NavLink></span>)}
           </div>
           <div className="externalLinks">
-            {this.props.data.externalLinks.liveVersion ? <a href={this.props.data.externalLinks.liveVersion} target="_blank" rel="noopener noreferrer"><span><img src="/img/icons/live.png" alt="BFTawfik Twitter Bot 2"/></span></a> : null}
-            {this.props.data.externalLinks.github ? <a href={this.props.data.externalLinks.github} target="_blank" rel="noopener noreferrer"><span><img src="/img/icons/projectGithub.png" alt="BFTawfik Twitter Bot 2"/></span></a> : null}
+            {this.props.data.externalLinks.liveVersion ? <a href={this.props.data.externalLinks.liveVersion} target="_blank" rel="noopener noreferrer"><span><img src="/img/icons/live.png" alt="live link icon"/></span></a> : null}
+            {this.props.data.externalLinks.github ? <a href={this.props.data.externalLinks.github} target="_blank" rel="noopener noreferrer"><span><img src="/img/icons/projectGithub.png" alt="github icon"/></span></a> : null}
           </div>
         </div>
       </div>
