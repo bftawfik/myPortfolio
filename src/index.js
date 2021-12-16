829a495a144df17f143162198e4de73786fce34d
bftawfik <bftawfik@gmail.com>
2021-12-16 22:51:01 +0200

    add shared styles and move index style to root

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App/App";
 import "./styles/vr.scss";
-import "./styles/Index.sass";
+import "./index.scss";
 
 import * as serviceWorker from "./serviceWorker";
 ReactDOM.render(<App />, document.getElementById("root"));
